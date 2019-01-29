92850d4de62ed3abdc3139bc7d324599b61d7679
Tiger <litianyue4441@163.com>
2019-01-29 19:12:04 +0800

    1.29

---
+++ translate.js
@@ -0,0 +1,74 @@
+subscript //下标
+warning
+error
+syntax	//语法
+indirection
+direct
+differ	//不同
+level
+void //空的
+int(integer) //整数
+long
+double
+short
+char
+include //包括包含
+stream //河流
+string //字符串
+const
+public
+private
+main
+struct
+class
+function
+non //非
+interactive //互动的，交互式的
+version
+argument //参数
+reset //重置
+restore //恢复
+value
+contents //内容
+namespace //命名空间
+item
+array //队列，数组
+elements //元素
+compute
+extern //外来的
+while
+do
+for
+case
+switch
+continue
+break
+return
+clear
+file
+float
+pointer //指针
+template //模板
+type //类型
+primer //入门书
+plus //加
+edition //版本
+new
+delete
+abstract //抽象的
+data
+linux
+standard //标准
+program
+software
+hardware
+language
+object //对象
+code
+auto
+static
+visual //可视化
+source //根源
+integrate //使整合
+development
+enviorment
