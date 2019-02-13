ccf02f91c42a0150f1070a1504fbcdc6e4574750
Litianyue <litianyue4441@163.com>
2019-02-13 22:02:19 +0800

    myfir

---
+++ CCGame/assets/resource/scripts/scene.js
@@ -0,0 +1,71 @@
+// Learn cc.Class:CC的类
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+//  
+// Learn Attribute:学习属性
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+//  
+// Learn life-cycle callbacks:生命周期回调
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad:function() {
+    	var logo, s_max, s_min, seq, rf;
+    	logo = cc.find("UI_ROOT/start_Button");
+    	s_max = cc.scaleTo(1.5, 1.1); 
+    	s_min = cc.scaleTo(1.5, 0.9);
+    	seq = cc.sequence([s_max, s_min]);
+    	rf = cc.repeatForever(seq);
+    	logo.runAction(rf);
+
+    	logo = cc.find("UI_ROOT/boom_1");
+    	s_max = cc.scaleTo(2.4, 1.1); 
+    	s_min = cc.scaleTo(2.4, 0.9);
+    	seq = cc.sequence([s_max, s_min]);
+    	rf = cc.repeatForever(seq);
+    	logo.runAction(rf);
+
+    	logo = cc.find("UI_ROOT/boom_2");
+    	s_max = cc.scaleTo(3.5, 1.4); 
+    	s_min = cc.scaleTo(2.5, 0.9);
+    	seq = cc.sequence([s_max, s_min]);
+    	rf = cc.repeatForever(seq);
+    	logo.runAction(rf);
+
+    	var start_Button = cc.find("UI_ROOT/start_Button")
+    	var rot = cc.rotateBy(1.2,360).easing(cc.easeCubicActionIn());
+    	rf = cc.repeatForever(rot);
+    	start_Button.runAction(rf);
+
+    },
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
---
+++ Question.js
@@ -0,0 +1,2 @@
+1:缓动运动:easing(cc.easeCubicActionIn());这些是怎么找到的，如何读懂API；
+2:代码中多次给s_min等赋值，为何不会互相覆盖。
